94ae2a768b361435a455f6f67ab64671063d00c7
Chris Dosé <chris.dose@gmail.com>
2019-09-27 17:55:36 -0700

    always display hostname

--- fish/init.fish
+++ fish/init.fish
@@ -1,5 +1,5 @@
 set -g theme_display_asdf "no"
-set -g theme_display_user "no"
+set -g theme_display_user "yes"
 set -g theme_hood_ornament "λ"
 
 set -gx PATH ~/.cargo/bin ./bin ~/bin ./node_modules/.bin $PATH
